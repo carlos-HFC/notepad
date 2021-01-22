21591d005a5b85fc03e7bf0ae40a1b949f98fb04
Carlos Faustino <chfcchfc96@gmail.com>
2021-01-22 11:06:59 -0300

    Temas do app

---
+++ frontend/src/css/themes/index.ts
@@ -0,0 +1,21 @@
+const light = {
+  title: "light",
+  background: "#f5f5f5",
+  text: "#343a40",
+  primary: "#5C0094",
+  checked: "#dedede",
+  shadow: "0,0,0",
+  borderChecked: "#ccc"
+}
+
+const dark = {
+  title: "dark",
+  background: "#222",
+  text: "#fff",
+  primary: "#343a40",
+  checked: "#666",
+  shadow: "255,255,255",
+  borderChecked: "#999"
+}
+
+export { dark, light }
