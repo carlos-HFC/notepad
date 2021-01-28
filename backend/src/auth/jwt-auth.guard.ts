b33abd77e4372c926695241fbcab4bdbf80ca0c6
Carlos Faustino <chfcchfc96@gmail.com>
2021-01-28 18:31:33 -0300

    Sem token, o status retornado é 422

--- backend/src/auth/jwt-auth.guard.ts
+++ backend/src/auth/jwt-auth.guard.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from "@nestjs/passport";
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   handleRequest(err, token, info) {
-    if (!token) throw new HttpException("Você não tem acesso", 404)
+    if (!token) throw new HttpException("Você não tem acesso", 422)
     return token
   }
 }
