468b85d516c82c260b625565885576cef2e418fd
Carlos Faustino <chfcchfc96@gmail.com>
2021-01-29 18:20:33 -0300

    Identação

--- frontend/src/components/InputBlock.tsx
+++ frontend/src/components/InputBlock.tsx
@@ -5,37 +5,41 @@ import { FaEyeSlash, FaEye } from "react-icons/fa"
 import { Button } from "."
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-   label: string
-   pass?: boolean
+  label: string
+  pass?: boolean
 }
 
 const InputBlock: React.FC<InputProps> = ({ label, pass, ...props }) => {
-   const [type, setType] = useState("password")
+  const [type, setType] = useState("password")
 
-   return (
-      <div className="row w-75 mb-2">
-         <div className="col-12 mb-2">
-            <label htmlFor={props.id}>{label}</label>
-            {!pass
-               ? <><input className="form-control" {...props} /><span /></>
-               : (
-                  <>
-                     <div className="input-group">
-                        <input className="form-control password" type={type} {...props} />
-                        <span />
-                        <div className="input-group-append">
-                           {type === 'password'
-                              ? <Button background="gray" type="button" label={<FaEye />} onClick={() => setType("text")} />
-                              : <Button background="gray" type="button" label={<FaEyeSlash />} onClick={() => setType("password")} />
-                           }
-                        </div>
-                     </div>
-                  </>
-               )
-            }
-         </div>
+  return (
+    <div className="row w-75 mb-2">
+      <div className="col-12 mb-2">
+        <label htmlFor={props.id}>{label}</label>
+        {!pass
+          ? <><input className="form-control" {...props} /><span /></>
+          : (
+            <>
+              <div className="input-group">
+                <input className="form-control password" type={type} {...props} />
+                <span />
+                <div className="input-group-append">
+                  {type === 'password'
+                    ? <Button background="gray" type="button" label={<FaEye />}
+                      onClick={() => setType("text")}
+                    />
+                    : <Button background="gray" type="button" label={<FaEyeSlash />}
+                      onClick={() => setType("password")}
+                    />
+                  }
+                </div>
+              </div>
+            </>
+          )
+        }
       </div>
-   )
+    </div>
+  )
 }
 
 export default InputBlock
--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -1,19 +1,19 @@
 import { HashLoader } from 'react-spinners'
 
 interface LoaderProps {
-   loading: boolean
+  loading: boolean
 }
 
 const Loader: React.FC<LoaderProps> = ({ loading }) => {
-   return (
-      loading ? (
-         <div className="first">
-            <div>
-               <HashLoader loading={loading} color="white" size={150} />
-            </div>
-         </div>
-      ) : <></>
-   )
+  return (
+    loading ? (
+      <div className="first">
+        <div>
+          <HashLoader loading={loading} color="white" size={150} />
+        </div>
+      </div>
+    ) : <></>
+  )
 }
 
 export default Loader
