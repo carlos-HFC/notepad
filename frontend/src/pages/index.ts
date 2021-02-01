d7f0fbe67f8b906cb522c1728eb152c2898ec014
Carlos Faustino <chfcchfc96@gmail.com>
2021-02-01 18:37:08 -0300

    .

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,9 +1,11 @@
 import AddNote from './AddNote'
 import Login from './Login'
 import MyNotes from './MyNotes'
+import Profile from './Profile'
 
 export {
   AddNote,
   Login,
   MyNotes,
+  Profile
 }
