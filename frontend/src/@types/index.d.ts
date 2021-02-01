6d24c3b7902f8b8a1a2774e528fecc9298de4d00
Carlos Faustino <chfcchfc96@gmail.com>
2021-02-01 18:38:33 -0300

    Ajustar nome das interfaces, novas props no styled-components]

--- frontend/src/@types/index.d.ts
+++ frontend/src/@types/index.d.ts
@@ -9,14 +9,27 @@ declare module "styled-components" {
     checked: string
     shadow: string
     borderChecked: string
+    perfilBg: string
+    perfilBgList: string
+    perfilText: string
   }
 }
 
-export interface Notes {
+export interface INotes {
   id: number
   userId: number
   title: string
   description: string
   createdAt: Date
   updatedAt: Date
+}
+
+export interface IUser {
+  id: number
+  name: string
+  email: string
+  hash: string
+  createdAt: Date
+  updatedAt: Date
+  deletedAt: Date | null
 }
