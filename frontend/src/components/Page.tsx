4c29e84331c48443deb75eb306a8831b2a0fb80d
Carlos Faustino <chfcchfc96@gmail.com>
2021-02-01 18:33:36 -0300

    title

--- frontend/src/components/Page.tsx
+++ frontend/src/components/Page.tsx
@@ -6,7 +6,7 @@ const Page: React.FC<PageProps> = ({ title, children }) => {
   return (
     <main className="m-3">
       <div className="container p-3">
-        <h2 className="text-center mb-3" title={title}>
+        <h2 className="text-center font-weight-bold mb-3" title={title}>
           {title}
         </h2><hr />
         <section className="mt-4">
