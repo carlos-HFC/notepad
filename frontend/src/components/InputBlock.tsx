6dd3bb2c109a4f3710b6ee4977b5d296a0430e3c
Carlos Faustino <chfcchfc96@gmail.com>
2021-02-01 18:33:15 -0300

    Novas props

--- frontend/src/components/InputBlock.tsx
+++ frontend/src/components/InputBlock.tsx
@@ -7,22 +7,24 @@ import { Button } from "."
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string
   pass?: boolean
+  width?: string
+  col?: string
 }
 
-const InputBlock: React.FC<InputProps> = ({ label, pass, ...props }) => {
+const InputBlock: React.FC<InputProps> = ({ label, pass, width = "", col = "col-12", ...props }) => {
   const [type, setType] = useState("password")
 
   return (
-    <div className="row w-75 mb-2">
-      <div className="col-12 mb-2">
+    <div className={`row mb-2 ${width}`}>
+      <div className={`${col} mb-2`}>
         <label htmlFor={props.id}>{label}</label>
         {!pass
-          ? <><input className="form-control" {...props} /><span /></>
+          ? <><input className="form-control" {...props} /><span className="inputbd" /></>
           : (
             <>
               <div className="input-group">
                 <input className="form-control password" type={type} {...props} />
-                <span />
+                <span className="inputbd" />
                 <div className="input-group-append">
                   {type === 'password'
                     ? <Button background="gray" type="button" label={<FaEye />}
