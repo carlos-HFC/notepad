cf82537b627b3d65ad96d30969f02b181302778f
Carlos Faustino <chfcchfc96@gmail.com>
2021-02-01 18:37:37 -0300

    Nova página (Profile)

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { DefaultTheme, ThemeProvider } from 'styled-components'
 
-import { AddNote, Login, MyNotes } from './pages'
+import { AddNote, Login, MyNotes, Profile } from './pages'
 import { Header } from './components'
 import { isAuth } from './services/auth'
 
@@ -10,9 +10,10 @@ import { dark, light } from './css/themes'
 import GlobalStyle from './css/styles'
 
 function Private({ ...props }) {
-  const [theme, setTheme] = useState<DefaultTheme>(JSON.parse(localStorage.getItem("THEME") as string) || light)
+  // const [theme, setTheme] = useState<DefaultTheme>(JSON.parse(localStorage.getItem("THEME") as string) || light)
+  const [theme, setTheme] = useState<DefaultTheme>(dark)
 
-  useEffect(() => localStorage.setItem("THEME", JSON.stringify(theme)), [theme])
+  // useEffect(() => localStorage.setItem("THEME", JSON.stringify(theme)), [theme])
 
   const handleTheme = () => setTheme(theme.title === 'light' ? dark : light)
 
@@ -24,7 +25,7 @@ function Private({ ...props }) {
       <Header handleTheme={handleTheme} />
       <Route path="/note" component={AddNote} />
       <Route path="/mynote" component={MyNotes} />
-      <Route path="/profile" component={MyNotes} />
+      <Route path="/profile" component={Profile} />
     </ThemeProvider>
   )
 }
