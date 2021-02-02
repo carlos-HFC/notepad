1caa9ef8478baca8dcdf6a91e0fb517d73e4e40e
Carlos Faustino <chfcchfc96@gmail.com>
2021-02-02 13:28:54 -0300

    Remoção de função não utilizada

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -1,5 +1,4 @@
 export const isAuth = () => localStorage.getItem("TOKEN") !== null
-export const getToken = () => localStorage.getItem("TOKEN")
 export const setToken = (token: string) => localStorage.setItem("TOKEN", token)
 export const logout = () => {
   localStorage.removeItem("TOKEN")
