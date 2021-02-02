dc5253634d81dabd913262bc1bf16fe31dbd864b
Carlos Faustino <chfcchfc96@gmail.com>
2021-02-02 13:28:13 -0300

    Novo retorno de mensagem

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -19,4 +19,29 @@ const notification = (className: 'success' | 'danger', icon: SweetAlertIcon, tit
   }).fire(title, undefined, icon)
 }
 
-export { notification }
+const confirmation = (title: string, text: string, confirm: string, cancel: string) => {
+  return Swal.fire({
+    title,
+    text,
+    confirmButtonText: confirm,
+    cancelButtonText: cancel,
+    icon: 'warning',
+    showCancelButton: true,
+    showClass: {
+      popup: "animate__animated animate__fadeInLeftBig"
+    },
+    hideClass: {
+      popup: "animate__animated animate__fadeOutLeftBig"
+    },
+    confirmButtonColor: "#1d6f42",
+    cancelButtonColor: "#c0392b",
+    customClass: {
+      popup: 'notification-warning',
+      title: 'title-warning',
+      confirmButton: 'outline-none',
+      cancelButton: 'outline-none'
+    },
+  })
+}
+
+export { confirmation, notification }
