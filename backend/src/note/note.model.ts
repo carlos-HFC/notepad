724eb032cdb90b2303db38861726f6f9c002535d
Carlos Faustino <chfcchfc96@gmail.com>
2021-02-02 13:24:53 -0300

    Validação do Sequelize removida

--- backend/src/note/note.model.ts
+++ backend/src/note/note.model.ts
@@ -1,17 +1,15 @@
-import { BelongsTo, Column, DataType, ForeignKey, Model, NotEmpty, Scopes, Table } from "sequelize-typescript";
+import { BelongsTo, Column, DataType, ForeignKey, Model, Table } from "sequelize-typescript";
 
 import { User } from "src/user/user.model";
 
 @Table({ paranoid: true })
 export class Note extends Model<Note> {
-  @NotEmpty({ msg: "O título é obrigatório" })
   @Column({
     type: DataType.STRING,
     allowNull: false
   })
   title: string
 
-  @NotEmpty({ msg: "A descrição é obrigatória" })
   @Column({
     type: DataType.TEXT,
     allowNull: false
